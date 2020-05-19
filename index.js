7fdf70b1fae42f4fadead58ed5d99f4569796521
richen <richenlin@gmail.com>
2020-05-19 09:28:17 +0800

    think_webservice@1.0.0

---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * @ author: richen
+ * @ copyright: Copyright (c) - <richenlin(at)gmail.com>
+ * @ license: MIT
+ * @ version: 2020-05-19 09:27:32
+ */
+
+const soap = require('soap');
+const helper = require('think_lib');
+var _WS_CLIENTS = {};
+
+/**
+ * 创建webservice client
+ *
+ * @param {*} url
+ * @param {*} [options={}]
+ * @returns
+ */
+const createWS = function (url, options = {}) {
+    options = Object.assign({
+        endpoint: url,
+        description: '',
+        wsdl_options: { timeout: 10000 }
+    }, options);
+    let deferred = helper.getDefer();
+    soap.createClient(url, options, function (err, client) {
+        if (err) {
+            if (err.message && helper.toString(err.message).indexOf('TIMEDOUT') > -1) {
+                deferred.reject({ code: 504, message: err.message || 'WEBSERVICE服务实例化失败' });
+            } else {
+                deferred.reject({ code: 503, message: err.message || 'WEBSERVICE服务实例化失败' });
+            }
+        }
+        deferred.resolve(client);
+    });
+    return deferred.promise;
+};
+
+module.exports = async function (url, methods, params, options = {}, timeout = 10000) {
+    const key = `WS_${url}`;
+    if (!_WS_CLIENTS[key] || !_WS_CLIENTS[key][methods]) {
+        _WS_CLIENTS[key] = await createWS(url, options);
+    }
+
+    const defer = helper.getDefer();
+    _WS_CLIENTS[key][methods](params, function (err, response) {
+        if (err) {
+            if (err.message && helper.toString(err.message).indexOf('TIMEDOUT') > -1) {
+                defer.reject({ code: 504, message: err.message || 'WEBSERVICE服务调用失败' });
+            } else {
+                _WS_CLIENTS[key] = null;
+                defer.reject({ code: 503, message: err.message || 'WEBSERVICE服务调用失败' });
+            }
+        }
+        defer.resolve(response);
+    }, { timeout: timeout });
+    return defer.promise;
+};
